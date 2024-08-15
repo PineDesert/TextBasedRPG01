e0c246c6ef761c83f08a06be22bc18aec10b8ff6
Joe <Joe@DESKTOP-TC01QG6>
2024-08-15 14:22:28 +0100

    Add project files.

---
+++ TextBasedRPG01/Page.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRPG01
+{
+    internal class Page
+    {
+        public int PageNumber { get; private set; }
+        
+        public string Content { get; private set; }
+
+        public Page(int pageNumber, string content)
+        {
+            PageNumber = pageNumber;
+            Content = content;
+        }
+    }
+}
